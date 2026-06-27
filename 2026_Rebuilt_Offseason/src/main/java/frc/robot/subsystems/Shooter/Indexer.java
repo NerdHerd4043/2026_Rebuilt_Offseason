3fae2d1f79972e46fcc44720c4202abab543842f
Michael2121212121 <michaeldillman25@gmail.com>
2026-06-26 20:42:42 -0700

    Name Fix

---
+++ 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Shooter/Indexer.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.Shooter;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public class Indexer extends SubsystemBase {
+    private SparkMax indexerMoter = new SparkMax(0, null);
+
+    public Indexer() {
+        final SparkMaxConfig indexerMotorConfig = new SparkMaxConfig();
+
+        indexerMotorConfig.idleMode(IdleMode.kBrake);
+
+        indexerMoter.configure(indexerMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    }
+
+    public Command index() {
+        return this.run(() -> {
+            indexerMoter.set(0.1);
+        }).finallyDo(() -> {indexerMoter.stopMotor;});
+    }
+}
