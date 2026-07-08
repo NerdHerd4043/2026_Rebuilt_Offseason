98c15e31c5db19497b4f6e2a45d1e66bb1fd875e
Michael2121212121 <michaeldillman25@gmail.com>
2026-07-07 19:20:53 -0700

    Intaking motor set up

--- 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,5 +1,32 @@
 package frc.robot.subsystems.Intake;
 
-public class Intake {
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+
+public class Intake extends SubsystemBase {
+    private SparkMax intakingMotor = new SparkMax(IntakeConstants.intakingMotorID, MotorType.kBrushless);
+
+    public Intake() {
+        final SparkMaxConfig intakingMotorConfig = new SparkMaxConfig();
+
+        intakingMotorConfig.idleMode(IdleMode.kBrake);
+
+        intakingMotor.configure(intakingMotorConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+    }
+
+    public Command runIntake() {
+        return this.run(() -> {
+            intakingMotor.set(IntakeConstants.intakeSpeed);
+        }).finallyDo(() -> {
+            intakingMotor.stopMotor();
+        });
+    }
 }
--- 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.Intake;
 
 public class IntakeConstants {
+    public static final int intakingMotorID = 20;
 
+    public static final double intakeSpeed = 0.2;
 }
