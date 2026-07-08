9c7e66c995be16408df0ceb3d3f8094dafe33d32
Michael2121212121 <michaeldillman25@gmail.com>
2026-07-07 21:46:17 -0700

    Intake articulating pid

--- 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -1,25 +1,41 @@
 package frc.robot.subsystems.Intake;
 
+import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 
 public class Intake extends SubsystemBase {
     private SparkMax intakingMotor = new SparkMax(IntakeConstants.intakingMotorID, MotorType.kBrushless);
+    private SparkFlex articulatingMotor = new SparkFlex(IntakeConstants.articulatingMotorID, MotorType.kBrushless);
+
+    private AbsoluteEncoder absEncoder;
+
+    private PIDController pidController = new PIDController(IntakeConstants.p, IntakeConstants.i, IntakeConstants.d);
 
     public Intake() {
         final SparkMaxConfig intakingMotorConfig = new SparkMaxConfig();
+        final SparkFlexConfig articulatingMotorConfig = new SparkFlexConfig();
 
         intakingMotorConfig.idleMode(IdleMode.kBrake);
+        articulatingMotorConfig.idleMode(IdleMode.kBrake);
 
         intakingMotor.configure(intakingMotorConfig, ResetMode.kResetSafeParameters,
                 PersistMode.kPersistParameters);
+        articulatingMotor.configure(articulatingMotorConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+
+        pidController.disableContinuousInput();
     }
 
     public Command runIntake() {
@@ -28,5 +44,13 @@ public class Intake extends SubsystemBase {
         }).finallyDo(() -> {
             intakingMotor.stopMotor();
         });
+    }
+
+    @Override
+    public void periodic() {
+
+        articulatingMotor.setVoltage(pidController.calculate(absEncoder.getPosition() * 360));
+
+        super.periodic();
     }
 }
--- 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ 2026_Rebuilt_Offseason/src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -2,6 +2,11 @@ package frc.robot.subsystems.Intake;
 
 public class IntakeConstants {
     public static final int intakingMotorID = 20;
+    public static final int articulatingMotorID = 21;
 
     public static final double intakeSpeed = 0.2;
+
+    public static final double p = 0;
+    public static final double i = 0;
+    public static final double d = 0;
 }
